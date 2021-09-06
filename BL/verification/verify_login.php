0af7afc4cfafc7c72383cb3627f4095b62150f5e
Christian Steentoft <chrsteentoft@gmail.com>
2021-09-06 11:01:45 +0200

    login edits

--- BL/verification/verify_login.php
+++ BL/verification/verify_login.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+include '../dbConnections/dbConnection.php';
+global $conn;
+
 function ExtendedAddslash(&$params)
 {
     foreach ($params as &$var)
@@ -8,11 +11,6 @@ function ExtendedAddslash(&$params)
     }
 }
 ExtendedAddslash($_POST);
-
-$conn = mysqli_connect("localhost", "root", "", "volleyball");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
@@ -24,6 +22,7 @@ if ($stmt->execute()){
     $result = $stmt->get_result();
     if (mysqli_num_rows($result) > 0) {
         $_SESSION['volleyball_validation'] = true;
+        $_SESSION['volleyball_username'] = $username;
         echo "true";
     } else {
         echo "false";
---
+++ templates/adminHeader.php
@@ -0,0 +1,50 @@
+<?php session_start() ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/import.css">
+
+    <title>Håndbold</title>
+</head>
+<body>
+
+<div class="logo-container container">
+    <img class="logo" src="../billeder/logo.png">
+</div>
+<div class="container container-custom">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="./">Forside</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="omklubben.php">Om klubben</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="staevneplan.php">Stævneplan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tilmelding.php">Tilmelding</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="galleri.php">Galleri</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="bestyrelse.php">Bestyrelse</a>
+                </li>
+            </ul>
+            <div class="my-2 my-lg-0">
+                <p>Velkommen <?php echo $_SESSION['volleyball_username']; ?></p>
+            </div>
+        </div>
+    </nav>
