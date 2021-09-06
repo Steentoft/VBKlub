5a85e67fd7da8e64779c3674cc9a1dcb9acb9dd0
Christian Steentoft <chrsteentoft@gmail.com>
2021-09-06 12:09:44 +0200

    adminpanel index

--- index.php
+++ index.php
@@ -3,44 +3,19 @@
 
 
 <div class="row content">
-        <div class="col-lg">
-            <img src="billeder/image6.jpeg" style="width: 100%">
-        </div>
-        <div class="col-lg">
-            <h3>Overskrift Lorem ipsum</h3>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-        </div>
+    <div class="col-lg">
+        <img src="billeder/image6.jpeg" style="width: 100%">
+    </div>
+    <div class="col-lg">
+        <h3>Overskrift Lorem ipsum</h3>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
     </div>
 </div>
 
-<!-- JavaScript -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script>
 
-$('#login').click(function (){
-    let username = $('#username').val();
-    let password = $('#password').val();
 
-    $.post("BL/verification/verify_login.php",
-        {
-            username: username,
-            password: password
-        },
-        function(data){
-            if (data == "true"){
-                window.location.href = "adminpanel/";
-            } else {
-                alert('Forkert brugernavn eller kodeord.')
-            }
-        });
 
-});
 
-</script>
-</body>
-</html>
+<?php include "templates/footer.php"; ?>
