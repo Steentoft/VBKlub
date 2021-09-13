7e7e4bbc34f8187c0c18365ec7bd738e02a3675d
Mathias Duus <mathias031001@gmail.com>
2021-09-13 13:26:24 +0200

    Verifies login with password_verify()

--- BL/verification/verify_login.php
+++ BL/verification/verify_login.php
@@ -16,13 +16,19 @@ $username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = mysqli_real_escape_string($conn, $_POST['password']);
 
 
-$stmt = $conn->prepare("SELECT * FROM administrators WHERE username=? AND password=?");
-$stmt->bind_param("ss", $username, $password);
+$stmt = $conn->prepare("SELECT * FROM administrators WHERE username=?");
+$stmt->bind_param("s", $username);
 if ($stmt->execute()){
     $result = $stmt->get_result();
     if (mysqli_num_rows($result) > 0) {
-        $_SESSION['volleyball_validation'] = true;
-        header('Location: ../../adminpanel');
+        $hashedPassword = $result->fetch_assoc()['password'];
+        if (password_verify($password, $hashedPassword)) {
+            $_SESSION['volleyball_validation'] = true;
+            header('Location: ../../adminpanel');
+        }
+        else{
+            echo "password failed: ".password_verify($password, $hashedPassword);
+        }
     } else {
         echo "false";
     }
