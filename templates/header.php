26e72efc8e509e7432a1b47b98dbb7f9c956909c
Christian Steentoft <chrsteentoft@gmail.com>
2021-09-15 09:21:24 +0200

    icon.ico

--- templates/header.php
+++ templates/header.php
@@ -5,6 +5,7 @@ session_start();
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="icon" type="image/png" href="billeder/icon.ico">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
